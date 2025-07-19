7ebffc7cd79d88a34c4cdf453ac74f23ac007f06
Jason Hoyt <gva-jason_hoyt@pfm.org>
2025-07-19 19:07:25 -0400

    new servies

--- src/services/useAdmin.tsx
+++ src/services/useAdmin.tsx
@@ -4,6 +4,18 @@ import axios from "axios";
 
 export const useAdmin = (): {
     getStates: () => Promise<any[]>
+    postAdmin: (
+        firstName: string,
+        lastName: string,
+        companyName: string,
+        email: string,
+        password: string,
+        streetAddress: string,
+        city: string,
+        state: string,
+        zipcode: string,
+        phoneNumber: string
+    ) => Promise<any>
 } => {
     const { endpoint } = useConfig();
 
@@ -15,6 +27,38 @@ export const useAdmin = (): {
             return e.response.data;
         }
     }
+    const postAdmin = async (
+        firstName: string,
+        lastName: string,
+        companyName: string,
+        email: string,
+        password: string,
+        streetAddress: string,
+        city: string,
+        state: string,
+        zipcode: string,
+        phoneNumber: string
+    ) => {
+        try {
+            const { data } = await axios.post(`${endpoint}admin`, {
+                first_name: firstName,
+                last_name: lastName,
+                company_name: companyName,
+                email,
+                password,
+                address: {
+                    street_address: streetAddress,
+                    city,
+                    state,
+                    zipcode
+                },
+                phone_number: phoneNumber
+            });
+            return data
+        } catch (e: any) {
+            return e.response.data
+        }
+    }
 
-    return { getStates };
+    return { getStates, postAdmin };
 };
--- src/services/useLogin.tsx
+++ src/services/useLogin.tsx
@@ -3,62 +3,18 @@ import {useConfig} from "./useConfig";
 
 export const useLogin = (): {
     login: (email: string, password: string) => Promise<any>,
-    postAdmin: (
-        firstName: string,
-        lastName: string,
-        companyName: string,
-        email: string,
-        password: string,
-        streetAddress: string,
-        city: string,
-        state: string,
-        zipcode: string,
-        phoneNumber: string
-    ) => Promise<any>
 } => {
 
     const { endpoint } = useConfig()
 
-    return {
-        login: async (email: string, password: string) => {
-            try {
-                const { data } = await axios.post(`${endpoint}login`, { email, password })
-                return data
-            } catch (e: any) {
-                return e.response.data
-            }
-        },
-        postAdmin: async (
-            firstName: string,
-            lastName: string,
-            companyName: string,
-            email: string,
-            password: string,
-            streetAddress: string,
-            city: string,
-            state: string,
-            zipcode: string,
-            phoneNumber: string
-        ) => {
-            try {
-                const { data } = await axios.post(`${endpoint}admin`, {
-                    first_name: firstName,
-                    last_name: lastName,
-                    company_name: companyName,
-                    email,
-                    password,
-                    address: {
-                        street_address: streetAddress,
-                        city,
-                        state,
-                        zipcode
-                    },
-                    phone_number: phoneNumber
-                });
-                return data
-            } catch (e: any) {
-                return e.response.data
-            }
+    const login = async (email: string, password: string) => {
+        try {
+            const { data } = await axios.post(`${endpoint}login`, { email, password })
+            return data
+        } catch (e: any) {
+            return e.response.data
         }
     }
+
+    return { login }
 }
